ab1af74914f971cf9cf63a12ba3e18776054f853
Zahariuc David <d.zahariuc@codeur.online>
2022-08-19 09:32:47 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+$("#conseil").click(function() {
+    window.location.replace('conseil_rh.html');
+})
+$("#audit").click(function() {
+    window.location.replace('audit_organisationnel.html');
+})
+$("#gestion").click(function() {
+    window.location.replace('gestion.html');
+})
+$("#legal").click(function() {
+    window.location.replace('conformite_legale.html');
+})
+$("#gouvernance").click(function() {
+    window.location.replace('gouvernance_partagee.html');
+})
+$("#accompagnement").click(function() {
+    window.location.replace('accompagnement_individuel.html');
+})
