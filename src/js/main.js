aab7f32343de82f7fec7d3b61dbc4b5dae6cdecd
Zahariuc David <d.zahariuc@codeur.online>
2022-08-26 14:31:58 +0200

    create profile expertise pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,35 @@
 
 
-$("#conseil").click(function () {
-    window.location.replace('conseil_rh.html');
+$(".conseil").click(function () {
+    window.location.replace("/conseil_rh");
 })
-$("#audit").click(function () {
-    window.location.replace('audit_organisationnel.html');
+$(".audit").click(function () {
+    window.location.replace('/audit_organisationnel');
 })
-$("#gestion").click(function () {
-    window.location.replace('gestion.html');
+$(".gestion").click(function () {
+    window.location.replace('/gestion');
 })
-$("#legal").click(function () {
-    window.location.replace('conformite_legale.html');
+$(".legal").click(function () {
+    window.location.replace('/conformite_legale');
 })
-$("#gouvernance").click(function () {
-    window.location.replace('gouvernance_partagee.html');
+$(".gouvernance").click(function () {
+    window.location.replace('/gouvernance_partagee');
 })
-$("#accompagnement").click(function () {
-    window.location.replace('accompagnement_individuel.html');
+$(".accompagnement").click(function () {
+    window.location.replace('/accompagnement_individuel');
+})
+
+$(".asso").click(function () {
+    window.location.replace('/asso');
+})
+$(".particulier").click(function () {
+    window.location.replace('/particulier');
+})
+$(".rep_perso").click(function () {
+    window.location.replace('/rep_perso');
+})
+$(".entreprise").click(function () {
+    window.location.replace('/entreprise');
 })
 
 
