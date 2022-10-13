1a4c75bdb2af4a9eeeba640cf62b9708a0acb30e
unknown <davidzahariuc@gmail.com>
2022-10-13 12:26:00 +0200

    final mods v0.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,111 +1,111 @@
 
-
+/* Navigation between pages */
 $(".conseil").click(function () {
-    window.location.replace("/conseil_rh");
+    window.location.href="/conseil-rh";
 })
 $(".audit").click(function () {
-    window.location.replace('/audit_organisationnel');
+    window.location.href="/audit-organisationnel";
 })
 $(".gestion").click(function () {
-    window.location.replace('/gestion');
+    window.location.href="/gestion-rh";
 })
 $(".legal").click(function () {
-    window.location.replace('/conformite_legale');
+    window.location.href="/conformite-legale";
 })
 $(".gouvernance").click(function () {
-    window.location.replace('/gouvernance_partagee');
+    window.location.href="/gouvernance-partagee";
 })
 $(".accompagnement").click(function () {
-    window.location.replace('/accompagnement_individuel');
+    window.location.href="/accompagnement-individuel";
 })
 
 $(".asso").click(function () {
-    window.location.replace('/asso');
+    window.location.href="/association";
 })
 $(".particulier").click(function () {
-    window.location.replace('/particulier');
+    window.location.href="/particulier";
 })
 $(".rep_perso").click(function () {
-    window.location.replace('/rep_perso');
+    window.location.href="/representant-du-personnel";
 })
 $(".entreprise").click(function () {
-    window.location.replace('/entreprise');
+    window.location.href="/entreprise";
 })
 /*contact btn*/
 $(".contact_btn").click(function () {
-    window.location.replace("/contact");
+    window.location.href="/contact";
 })
 $(".send_btn").click(function () {
-    window.location.replace("/contact");
+    window.location.href="/contact";
 })
 
-/*  menu déroulant*/
 $(document).ready(function () {
-
+    /* Drop-down menu */
     $(".dropdown").hover(function () {
         var dropdownMenu = $(this).children(".dropdown-menu");
         if (dropdownMenu.is(":visible")) {
             dropdownMenu.parent().toggleClass("open");
         }
     });
-/**/
+    /**/
 
+    /* cards flip on hover function */
     $(".flip-card").hover(function () {
 
         $(this).find(".flip-card-back")
             .css('transform', 'perspective(none) rotateY(0)')
         $(this).find(".flip-card-front")
             .css('transform', 'perspective(none) rotateY(-180deg)')
-
     }, function () {
-
         $(this).find(".flip-card-front")
             .css('transform', 'perspective(none) rotateY(0deg)')
         $(this).find(".flip-card-back")
             .css('transform', 'perspective(none) rotateY(180deg)')
     })
+    /**/
 
-
-    
-        $(".contr_over").on("click", function(){
-             $('html, body').animate({
-            scrollTop:  $( ".bg_sable" ).height()
+    /* Back to top function */
+    $(".contr_over").on("click", function () {
+        $('html, body').animate({
+            scrollTop: $(".bg_sable").height()
         }, 50);
-        })
+    })
+    /**/
 });
 
 /* Validation de formulaire */
+/*
 let nom = document.getElementById('name');
 let erreurNom = document.getElementById('msgErreur');
 let form = document.getElementsByTagName('form')[0];
 let nomRegExp = new RegExp("^[A-Z][A-Za-z' ,!]{2,35}$");
 
 nom.addEventListener('input', function (event) {
-    if (nom.validity.valid  && nomRegExp.test(nom.value)) {
+    if (nom.validity.valid && nomRegExp.test(nom.value)) {
         erreurNom.textContent = '';
     } else {
-        afficherErreur ();
-  }
+        afficherErreur();
+    }
 });
 form.addEventListener('submit', function (event) {
-    if(!(nomRegExp.test(nom.value)) || !nom.validity.valid) {
+    if (!(nomRegExp.test(nom.value)) || !nom.validity.valid) {
         event.preventDefault();
 
-        afficherErreur ();
+        afficherErreur();
     }
-  });
+});
 
-  function afficherErreur () {
+function afficherErreur() {
     let exp2 = new RegExp("(?=.*?[0-9])");
     let exp3 = new RegExp(".{2,}");
     let message = "";
-    if(nom.validity.valueMissing) {
+    if (nom.validity.valueMissing) {
         message += 'Veuillez insérer votre nom et prénom';
     }
-     else if(exp2.test(nom.value)) {
+    else if (exp2.test(nom.value)) {
         message += 'Le nom ne doit pas contenir des chiffres';
     } else if (!exp3.test(nom.value)) {
         message += 'Le nom doit contenir au minimum 2 caractères';
     }
     erreurNom.innerHTML = message;
-}   
+}   */
